3af54ea76bbdde4e83d88f41ae348ae6340939f5
Michal J. Gasior <michaljgasior@gmail.com>
2021-10-18 17:42:42 +0200

    Chapter 2 - ingredients view

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:learning_dart/recipe_detail.dart';
 import 'recipe.dart';
 
 void main() {
@@ -41,7 +42,15 @@ class _MyHomePageState extends State<MyHomePage> {
             child: ListView.builder(
                 itemCount: Recipe.samples.length,
                 itemBuilder: (BuildContext context, int index) {
-                  return buildRecipeCard(Recipe.samples[index]);
+                  return GestureDetector(
+                    onTap: () {
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context) {
+                        return RecipeDetail(recipe: Recipe.samples[index]);
+                      }));
+                    },
+                    child: buildRecipeCard(Recipe.samples[index]),
+                  );
                 })));
   }
 
@@ -56,11 +65,11 @@ class _MyHomePageState extends State<MyHomePage> {
             children: [
               Image(image: AssetImage(recipe.imageUrl)),
               const SizedBox(height: 14.0),
-              Text(recipe.label, style: const TextStyle(
-                fontSize: 20.0,
-                fontWeight: FontWeight.w700,
-                fontFamily: 'Palatino'
-              )),
+              Text(recipe.label,
+                  style: const TextStyle(
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.w700,
+                      fontFamily: 'Palatino')),
             ],
           ),
         ));
--- lib/recipe.dart
+++ lib/recipe.dart
@@ -1,12 +1,32 @@
 class Recipe {
+  int servings;
+  List<Ingredient> ingredients;
   String label;
   String imageUrl;
 
-  Recipe(this.label, this.imageUrl);
+  Recipe(this.label, this.imageUrl, this.servings, this.ingredients);
 
   static List<Recipe> samples = [
-    Recipe("Spagetti", "assets/spageti.jpeg"),
-    Recipe("Prażony syr", "assets/syr.jpg"),
-    Recipe("Tacosy", "assets/tacosy.jpeg"),
+    Recipe("Spagetti", "assets/spageti.jpeg", 1, [
+      Ingredient(100, "grams", "Pasta"),
+      Ingredient(300, "grams", "Tomatoes")
+    ]),
+    Recipe("Prażony syr", "assets/syr.jpg", 5, [
+      Ingredient(150, "grams", "Gouda cheese"),
+      Ingredient(1, "box", "Bread crumbs")
+    ]),
+    Recipe("Tacosy", "assets/tacosy.jpeg", 7, [
+      Ingredient(7, "loaf", "Tortilla"),
+      Ingredient(1, "", "Salad"),
+      Ingredient(21, "", "Cherry tomatoes"),
+    ]),
   ];
+}
+
+class Ingredient {
+  double quantity;
+  String measure;
+  String name;
+
+  Ingredient(this.quantity, this.measure, this.name);
 }

c84ed91cdc75ae55963e320e3067dea89447ff04
Michal J. Gasior <michaljgasior@gmail.com>
2021-10-25 09:38:35 +0200

    Chapter 6 - new Navigator route

---
+++ lib/screens/grocery_item_screen.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:learning_dart/models/models.dart';
+
+class GroceryItemScreen extends StatefulWidget {
+  final Function(GroceryItem) onCreate;
+  final Function(GroceryItem) onUpdate;
+  final GroceryItem? originalItem;
+  final bool isUpdating;
+
+  const GroceryItemScreen(
+      {Key? key,
+      required this.onCreate,
+      required this.onUpdate,
+      this.originalItem})
+      : isUpdating = (originalItem != null),
+        super(key: key);
+
+  @override
+  _GroceryItemScreenState createState() => _GroceryItemScreenState();
+}
+
+class _GroceryItemScreenState extends State<GroceryItemScreen> {
+
+  // TODO: add grocery item screen state props
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO 12: Add Scaffold
+    return Container();
+  }
+}
--- lib/screens/grocery_screen.dart
+++ lib/screens/grocery_screen.dart
@@ -2,30 +2,40 @@ import 'package:flutter/material.dart';
 import 'package:learning_dart/models/models.dart';
 import 'package:learning_dart/screens/empty_grocery_screen.dart';
 import 'package:provider/provider.dart';
+import 'grocery_item_screen.dart';
 
 class GroceryScreen extends StatelessWidget {
   const GroceryScreen({Key? key}) : super(key: key);
 
   Widget buildGroceryScreen(BuildContext context) {
-    return Consumer<GroceryManager>(
-        builder: (context, manager, child) {
-          if (manager.groceryItems.isNotEmpty) {
-            return Container();
-          } else {
-            return const EmptyGroceryScreen();
-          }
-        }
-    );
+    return Consumer<GroceryManager>(builder: (context, manager, child) {
+      if (manager.groceryItems.isNotEmpty) {
+        return Container();
+      } else {
+        return const EmptyGroceryScreen();
+      }
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      floatingActionButton: FloatingActionButton(
-        child: const Icon(Icons.add),
-        onPressed: () {},
-      ),
-      body: buildGroceryScreen(context)
-    );
+        floatingActionButton: FloatingActionButton(
+          child: const Icon(Icons.add),
+          onPressed: () {
+            final manager = Provider.of<GroceryManager>(context, listen: false);
+            Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => GroceryItemScreen(
+                          onCreate: (item) {
+                            manager.addItem(item);
+                            Navigator.pop(context);
+                          },
+                          onUpdate: (item) {},
+                        )));
+          },
+        ),
+        body: buildGroceryScreen(context));
   }
 }

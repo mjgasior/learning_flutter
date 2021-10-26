76a08b8a86430a40cbca62b968a6521fdba376d2
Michal J. Gasior <michaljgasior@gmail.com>
2021-10-26 10:10:45 +0200

    Chapter 6 - ChoiceChip widgets

--- lib/screens/grocery_item_screen.dart
+++ lib/screens/grocery_item_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:learning_dart/models/models.dart';
 
 class GroceryItemScreen extends StatefulWidget {
@@ -20,12 +21,129 @@ class GroceryItemScreen extends StatefulWidget {
 }
 
 class _GroceryItemScreenState extends State<GroceryItemScreen> {
+  final _nameController = TextEditingController();
+  String _name = '';
+  Importance _importance = Importance.low;
+  DateTime _dueDate = DateTime.now();
+  TimeOfDay _timeOfDay = TimeOfDay.now();
+  Color _currentColor = Colors.green;
+  int _currentSliderValue = 0;
 
-  // TODO: add grocery item screen state props
+  @override
+  void initState() {
+    if (widget.originalItem != null) {
+      _nameController.text = widget.originalItem!.name;
+      _name = widget.originalItem!.name;
+      _currentSliderValue = widget.originalItem!.quantity;
+      _importance = widget.originalItem!.importance;
+      _currentColor = widget.originalItem!.color;
+      final date = widget.originalItem!.date;
+      _timeOfDay = TimeOfDay(hour: date.hour, minute: date.minute);
+      _dueDate = date;
+    }
+
+    _nameController.addListener(() {
+      setState(() {
+        _name = _nameController.text;
+      });
+    });
+
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
-    // TODO 12: Add Scaffold
-    return Container();
+    return Scaffold(
+        appBar: AppBar(
+          actions: [
+            IconButton(
+                onPressed: () {
+                  // TODO 24: add callback handler
+                },
+                icon: const Icon(Icons.check))
+          ],
+          elevation: 0.0,
+          title: Text('Grocery item',
+              style: GoogleFonts.lato(fontWeight: FontWeight.w600)),
+        ),
+        body: Container(
+          padding: const EdgeInsets.all(16.0),
+          child: ListView(
+            children: [
+              buildNameField(),
+              buildImportanceField(),
+              // TODO 15: date
+              // TODO 16: time
+              // TODO 17: color
+              // TODO 18: slider
+              // TODO 19: tile
+            ],
+          ),
+        ));
+  }
+
+  @override
+  void dispose() {
+    _nameController.dispose();
+    super.dispose();
+  }
+
+  Widget buildNameField() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text("Item name", style: GoogleFonts.lato(fontSize: 28.0)),
+        TextField(
+          controller: _nameController,
+          cursorColor: _currentColor,
+          decoration: InputDecoration(
+              hintText: 'E.g. apples, bananas',
+              enabledBorder: const UnderlineInputBorder(
+                  borderSide: BorderSide(color: Colors.white)),
+              focusedBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(color: _currentColor)),
+              border: UnderlineInputBorder(
+                  borderSide: BorderSide(color: _currentColor))),
+        )
+      ],
+    );
+  }
+
+  Widget buildImportanceField() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text("Importance", style: GoogleFonts.lato(fontSize: 28.0)),
+        Wrap(
+          spacing: 10.0,
+          children: [
+            ChoiceChip(
+                selectedColor: Colors.black,
+                selected: _importance == Importance.low,
+                label: const Text("low",
+                    style: TextStyle(color: Colors.white)),
+                onSelected: (selected) {
+                  setState(() => _importance = Importance.low);
+                }),
+            ChoiceChip(
+                selectedColor: Colors.black,
+                selected: _importance == Importance.medium,
+                label: const Text("medium",
+                    style: TextStyle(color: Colors.white)),
+                onSelected: (selected) {
+                  setState(() => _importance = Importance.medium);
+                }),
+            ChoiceChip(
+                selectedColor: Colors.black,
+                selected: _importance == Importance.high,
+                label: const Text("high",
+                    style: TextStyle(color: Colors.white)),
+                onSelected: (selected) {
+                  setState(() => _importance = Importance.high);
+                }),
+          ],
+        )
+      ],
+    );
   }
 }
